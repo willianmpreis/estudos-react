d3f712be0a85c34ac1bcad7ef05bbf7921b87888
Willian Reis <willian.reis@unifagoc.edu.br>
2020-08-08 14:08:23 -0300

    Dia 04 - mobile

---
+++ nextlevelweek-2/mobile/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import { AppLoading } from 'expo';
+import { useFonts, Archivo_400Regular, Archivo_700Bold } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
+
+export default function App() {
+  let [fontsLoader] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoader) {
+    return <AppLoading />
+  }
+  
+  return (
+    <>
+      <AppStack />
+      <StatusBar style="light" />
+    </>
+  );
+}
---
+++ nextlevelweek-2/mobile/src/pages/Example/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './style';
+
+function Exemple() {
+    return (
+    <View style={styles}>
+
+    </View>
+    );
+}
+
+export default Exemple;
